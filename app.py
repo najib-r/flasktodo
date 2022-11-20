493c982cd09e43e13164e6c79b25e17eb5188806
ysosrss <najib72645@gmail.com>
2022-11-20 13:01:18 +0800

    Add dynamic list

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import login_required
+from datetime import datetime
 import sqlite3
 
 
@@ -21,14 +22,40 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
+    # If a new item is submitted
+    if request.method == "POST":
+        text = request.form.get("todo")
+        # If text is not blank
+        if text:
+            # Get date and time
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            con = sqlite3.connect("database.db")
+            cur = con.cursor()
+            # get name of current user
+            res = cur.execute("SELECT username from USERS WHERE id = ?", (session["user_id"],))
+            name = res.fetchone()
+            # Add item to database
+            cur.execute("INSERT INTO items (username, item, time) VALUES (?, ?, ?)", (name[0], text, dt_string,))
+            con.commit()
+            # Get all items for current user to display
+            res = cur.execute("SELECT id, item FROM items WHERE username = ?", (name[0],))
+            rows = res.fetchall()
+            con.close()
+            return render_template("index.html", rows=rows)
     con = sqlite3.connect("database.db")
     cur = con.cursor()
-    res = cur.execute("SELECT username FROM users WHERE id = ?", (session["user_id"],))
-    row = res.fetchone()
-    return render_template("index.html", name=row[0])
+    # get name of current user
+    res = cur.execute("SELECT username from USERS WHERE id = ?", (session["user_id"],))
+    name = res.fetchone()
+    # Get all items for current user to display
+    res = cur.execute("SELECT id, item FROM items WHERE username = ?", (name[0],))
+    rows = res.fetchall()
+    con.close()
+    return render_template("index.html", rows=rows)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -97,3 +124,17 @@ def register():
         return redirect("/")
     # User reached via GET, so render register.html
     return render_template("register.html")
+
+# @app.route("/delete", methods=["POST"])
+# def delete():
+#     # Select item with id 
+#     id = request.form.get("id")
+#     if id :
+#         con = sqlite3.connect("database.db")
+#         cur = con.cursor()
+#         cur.execute("DELETE FROM items WHERE id = ?", (id,))
+#         con.commit()
+#         con.close()
+#     return redirect("/")
+
+
